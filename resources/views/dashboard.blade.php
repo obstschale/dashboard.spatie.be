febae515b06bb1622dd4ac02b25e831068e4ab6c
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2018-09-14 19:08:49 +0200

    New Dashboard order

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,13 @@
 
 @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
-<dashboard id="dashboard" columns="3" rows="3">
-    {{--<calendar position="a1"></calendar>--}}
-    <todoist team-member="hans-helge" position="a1"></todoist>
+<dashboard id="dashboard" columns="4" rows="4">
+    <tasks team-member="almut" position="a1:a2"></tasks>
+    <todoist team-member="hans-helge" position="a3:a4"></todoist>
+    <calendar position="d2:d3"></calendar>
     <time-weather position="b1" time-format="HH:mm:ss" date-format="ddd DD. MMMM" time-zone="Europe/Berlin" weather-city="Augsburg"></time-weather>
-    <tasks team-member="Fahrplan" position="c1"></tasks>
-    <music position="a2:c2"></music>
-    <tasks team-member="hans-helge" position="a3"></tasks>
-    <tasks team-member="almut" position="b3"></tasks>
+    <tasks team-member="Fahrplan" position="d1"></tasks>
+    <music position="b2:c2"></music>
     <uptime position="c3"></uptime>
     <internet-connection></internet-connection>
 </dashboard>

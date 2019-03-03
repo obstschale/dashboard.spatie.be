678b0e78ba8ccf8946a84d016141dbec8810f65c
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2019-03-03 18:24:17 +0100

    Colorize Speedtest information

--- resources/assets/js/components/Speedtest.vue
+++ resources/assets/js/components/Speedtest.vue
@@ -3,8 +3,8 @@
         <section class="speedtest">
             <div class="speedtest__last-test">
                 <div class="speedtest__icon h-background-icon"></div>
-                <div class="speedtest__upload">{{ speedtest.upload }} <span class="speedtest__mbits">Mbit/s</span></div>
-                <div class="speedtest__download">{{ speedtest.download }} <span class="speedtest__mbits">Mbit/s</span></div>
+                <div class="speedtest__upload" v-bind:class="[uploadColor]">{{ speedtest.upload }} <span class="speedtest__mbits">Mbit/s</span></div>
+                <div class="speedtest__download" v-bind:class="[downloadColor]">{{ speedtest.download }} <span class="speedtest__mbits">Mbit/s</span></div>
                 <div class="speedtest__date">{{ speedtest.date }}</div>
             </div>
         </section>
@@ -33,6 +33,32 @@ export default {
                 date: '',
             },
         };
+    },
+
+    computed: {
+        uploadColor() {
+            if (Number(this.speedtest.upload) >= 20) {
+                return 'speedtest__good'
+            }
+
+            if (Number(this.speedtest.upload) < 10) {
+                return 'speedtest__bad'
+            }
+
+            return 'speedtest__warning'
+        },
+
+        downloadColor() {
+            if (Number(this.speedtest.download) >= 75) {
+                return 'speedtest__good'
+            }
+
+            if (Number(this.speedtest.download) < 25) {
+                return 'speedtest__bad'
+            }
+
+            return 'speedtest__warning'
+        },
     },
 
     methods: {

812178332463c93777af7c1865cabc297f2e6664
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2020-10-22 16:59:22 +0200

    Add 5th calendar

--- config/calendars.php
+++ config/calendars.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'icals' => [ env('ICAL1'), env('ICAL2'), env('ICAL3'), env('ICAL4')],
+    'icals' => [ env('ICAL1'), env('ICAL2'), env('ICAL3'), env('ICAL4'), env('ICAL5')],
     'colors' => explode(',', env('ICAL_COLORS')),
 ];
